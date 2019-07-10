0f5fe38df03c5c29c0831416e76a848ae40fd4e0
MvonK <martin@kalab.sk>
2019-07-10 11:57:37 +0200

    Added support for some linux

--- install.py
+++ install.py
@@ -6,66 +6,70 @@ def first_run():
 	import unidecode
 	import unicodedata
 	
+	if platform == "windows":
+		os.system("wmic useraccount where name='%username%' get sid > sid.txt")
+		sid = open("sid.txt", "rb").read().decode("utf-16")[4:]
+		sid = sid[:-1]
 	
-	os.system("wmic useraccount where name='%username%' get sid > sid.txt")
-	sid = open("sid.txt", "rb").read().decode("utf-16")[4:]
-	sid = sid[:-1]
+		try:
+			import win32gui
+			import win32api
+			import win32con
+			import webbrowser
+			import unidecode
+		except:
+			input("hej, chybaju ti moduly. Mozem ich instalovat? Ak ano, stlac enter, ak nie, zavri program a nebudes ho mat proste")
+			os.system("pip install webbrowser")
+			os.system("pip install unidecode")
+			os.system("pip install pywin32")
+
+			os.execl(sys.executable, 'python', __file__, *sys.argv[1:])
+
+		fr = True
+		try:
+			os.mkdir('data')
+		except:
+			pass
+
+
+		name = input("Dobre rano, prve spustenie. Zadajte svoje meno ako je vo vysledkovkach pls (bez diakritiky): ")
+		fajl =  ET.parse("format.xml")
+		koren = fajl.getroot()
+		elm = koren.find(".//Command")
+		elm.text = sys.executable[:-4]+"w"+ sys.executable[-4:]
+		print("executable:", elm.text)
+		elm = koren.find(".//Arguments")
+		elm.text = os.path.abspath(os.path.dirname(__file__)) + "\\KS_checker.pyw" + " " + name
+		print("arguments:", elm.text)
+		elm = koren.find(".//WorkingDirectory")
+		elm.text = os.path.abspath(os.path.dirname(__file__)) + "\\data"
+		print("WD:", elm.text)
+		#adding user id for windows to accept it
+		elm = koren.find(".//UserId")
+		elm.text = sid
+		print("UserID:", elm.text)
 	
-	try:
-		import win32gui
-		import win32api
-		import win32con
-		import webbrowser
-		import unidecode
-	except:
-		input("hej, chybaju ti moduly. Mozem ich instalovat? Ak ano, stlac enter, ak nie, zavri program a nebudes ho mat proste")
-		os.system("pip install webbrowser")
-		os.system("pip install unidecode")
-		os.system("pip install pywin32")
-
-		os.execl(sys.executable, 'python', __file__, *sys.argv[1:])
-
-	fr = True
-	try:
-		os.mkdir('data')
-	except:
-		pass
+		koren.set("version", "1.2")
+		koren.set("xmlns", "http://schemas.microsoft.com/windows/2004/02/mit/task")
 
 
-	name = input("Dobre rano, prve spustenie. Zadajte svoje meno ako je vo vysledkovkach pls (bez diakritiky): ")
-	#xmlko = open("KSchecker.xml", "r").read()
-	#ako_text = ET.fromstring(xmlko)
-	fajl =  ET.parse("format.xml")
-	koren = fajl.getroot()
-	#print(xmlko)
-	elm = koren.find(".//Command")
-	elm.text = sys.executable[:-4]+"w"+ sys.executable[-4:]
-	print("executable:", elm.text)
-	elm = koren.find(".//Arguments")
-	elm.text = os.path.abspath(os.path.dirname(__file__)) + "\\KS_checker.pyw" + " " + name
-	print("arguments:", elm.text)
-	elm = koren.find(".//WorkingDirectory")
-	elm.text = os.path.abspath(os.path.dirname(__file__)) + "\\data"
-	print("WD:", elm.text)
-
-	elm = koren.find(".//UserId")
-	elm.text = sid
-	print("UserID:", elm.text)
-	
-	koren.set("version", "1.2")
-	koren.set("xmlns", "http://schemas.microsoft.com/windows/2004/02/mit/task")
+		fajl.write("scheduler_import.xml", xml_declaration = True, encoding = "utf-16")
+		#tostr = ET.tostring(koren, "utf-8")
+		print("running command...")
+		os.system('schtasks /create /xml "' + os.path.abspath(os.path.dirname(__file__)) + '\\scheduler_import.xml" /tn "KSchecker for ' + name + '"')
+		print("...end of command")
 
 
-	fajl.write("scheduler_import.xml", xml_declaration = True, encoding = "utf-16")
-	#tostr = ET.tostring(koren, "utf-8")
-	print("running command...")
-	os.system('schtasks /create /xml "' + os.path.abspath(os.path.dirname(__file__)) + '\\scheduler_import.xml" /tn "KSchecker for ' + name + '"')
-	print("...end of command")
+		os.chdir("data")
+		input("want to continue?")
+		return name
+	elif platform == "linux":
+		#here comes the instalation check
+		name = input("Dobre rano, prve spustenie. Zadajte svoje meno ako je vo vysledkovkach pls (bez diakritiky): ")
 
+		from crontab import CronTab
+		job = cron.new(command=os.path.abspath(os.path.dirname(__file__)) + "\\KS_checker.pyw" + " " + name)
 
-	os.chdir("data")
-	input("want to continue?")
-	return name
 
 if __name__ == '__main__':
 	first_run()
